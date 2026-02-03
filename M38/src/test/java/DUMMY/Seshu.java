4ff9a490ea4d5e501df6ca21bcb8f4a9d56d3284
yamnirddygmail.com <yamnirddygmail.com@localhost>
2026-02-03 16:52:04 +0530

    commit

---
+++ M38/src/test/java/DUMMY/Seshu.java
@@ -0,0 +1,11 @@
+package DUMMY;
+
+public class Seshu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi seshupriya");
+		System.out.println("Hi seshupriya");
+	}
+
+}
